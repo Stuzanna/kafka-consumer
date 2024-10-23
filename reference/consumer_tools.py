c97fde9620fd3c64dce7d19d315677ef068b865c
stuzanna <smould1011@gmail.com>
2024-10-23 09:02:32 +0100

    Refactor create deserializer

--- reference/consumer_tools.py
+++ reference/consumer_tools.py
@@ -1,16 +1,11 @@
 import json
-import requests
 
 from confluent_kafka.schema_registry.avro import AvroDeserializer
 from confluent_kafka.schema_registry.json_schema import JSONDeserializer
 
-# Create deserializer based on serialization format and schema location
-def load_schema(schema_loc: str, schema_file = None, schema_id = None, schema_registry_url = None) -> str:
+def load_schema(schema_loc: str, schema_file = None) -> str:
     """
-    Load a schema based on the provided schema location (local or remote).
-
-    This function loads a schema either from a local file or by fetching it 
-    from a remote schema registry, depending on the specified `schema_loc`.
+    Load a schema from a local file. Remote schemas can't be loaded from file.
 
     Parameters:
     ----------
@@ -18,10 +13,6 @@ def load_schema(schema_loc: str, schema_file = None, schema_id = None, schema_re
         The location of the schema. Must be either 'local' or 'remote'.
     schema_file : str, optional
         Path to the local schema file (required if `schema_loc` is 'local').
-    schema_id : int, optional
-        The ID of the schema to fetch from the schema registry (required if `schema_loc` is 'remote').
-    schema_registry_url : str, optional
-        The base URL of the schema registry (required if `schema_loc` is 'remote').
 
     Returns:
     -------
@@ -31,54 +22,68 @@ def load_schema(schema_loc: str, schema_file = None, schema_id = None, schema_re
     Raises:
     ------
     ValueError
-        If required arguments are missing or an invalid schema location is provided.
+        If schema_loc is not 'local' or if schema_file is missing.
     """
-    if schema_loc == 'local':
-        if schema_file is None:
-            raise ValueError("Schema file must be provided for local schemas.")
-        print(f'Loading schema from local file: {schema_file}')
-        with open(schema_file, 'r') as schema_file:
-            return json.dumps(json.load(schema_file))
-    elif schema_loc == 'remote':
-        if schema_registry_url is None:
-            raise ValueError("Schema registry URL must be provided for remote schemas.")
-        return None
-    else:
-        raise ValueError(f"Invalid schema location: {schema_loc}. Expected 'local' or 'remote'.")
+    if schema_loc != 'local':
+        raise ValueError("This function is only for local files.")
+    
+    if schema_file is None:
+        raise ValueError("Schema file must be provided for local schemas.")
+    
+    print(f'Loading schema from local file: {schema_file}')
+    with open(schema_file, 'r') as file:
+        return json.dumps(json.load(file))
 
-def create_deserializer(serialization, schema_str, schema_registry_client):
+def create_deserializer(serialization: str, schema_loc: str, schema_str: str = None, schema_registry_client = None):
     """
     Create a deserializer based on the specified serialization format.
-
-    Depending on the `serialization` parameter, this function creates and returns a 
-    JSON or Avro serializer using the provided schema string and Schema Registry client.
     If 'none' is specified, no serializer is created.
 
     Parameters:
     ----------
     serialization : str
         The serialization format to use. Expected values are 'json', 'avro', or 'none'.
-    schema_str : str
-        The schema in JSON format to use for serialization.
-    schema_registry_client : SchemaRegistryClient
+    schema_loc : str
+        The location of the schema ('local' or 'remote').
+    schema_str : str, optional
+        The schema in JSON format to use for local schema files.
+    schema_registry_client : SchemaRegistryClient, optional
         The client instance for interacting with the Schema Registry.
 
     Returns:
     -------
-    JSONSerializer, AvroSerializer, or None
-        A serializer for JSON or Avro based on the `serialization` type, or None if no serialization is selected.
+    Deserializer
+        A deserializer for JSON, Avro or no deserializer if none required.
 
     Raises:
     ------
     ValueError
-        If an invalid serialization type is provided.
+        If an invalid parameters provided or required are missing.
     """
     if serialization == 'json':
-        print("Creating JSON deserializer...")
-        return JSONDeserializer(schema_str)
+        if schema_loc == 'remote':
+            if schema_registry_client is None:
+                raise ValueError("SR client required for remote JSON deserialization.")
+            print("Creating JSON deserializer with remote schema...")
+            return JSONDeserializer(schema_registry_client=schema_registry_client)
+        else:
+            if schema_str is None:
+                raise ValueError("Schema string required for local JSON deserialization.")
+            print("Creating JSON deserializer with local schema...")
+            return JSONDeserializer(schema_str)
+    
     elif serialization == 'avro':
-        print("Creating Avro deserializer...")
-        return AvroDeserializer(schema_registry_client)
+        if schema_loc == 'remote':
+            if schema_registry_client is None:
+                raise ValueError("SR client required for remote Avro deserialization.")
+            print("Creating Avro deserializer...")
+            return AvroDeserializer(schema_registry_client)
+        else:
+            if schema_str is None:
+                raise ValueError("Schema string required for local JSON deserialization.")
+            print("Creating Avro deserializer with local schema...")
+            return AvroDeserializer(schema_str)
+        
     elif serialization == 'none':
         print('No serialization selected, skipping deserializer creation.')
         return None
